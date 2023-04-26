f4e8b795dfccba577bd8efcd5f098fe722b347cd
leirdan <uouleirdan@gmail.com>
2023-04-26 13:37:29 -0300

    first components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-project';
+  title = 'Movies, Series and Books!';
+  subtitle = 'Developed by @leirdan ;)'
 }
